1b56436bca7e0d88aa95e670834d0bf5509ce6ea
Paul Soria <dsoria@espol.edu.ec>
2023-01-06 20:18:08 -0500

    Modulo de cartas iniciar

---
+++ client/src/utils/cards.js
@@ -0,0 +1,31 @@
+const cards = [
+  {
+    content: "Descripcion",
+    type: "normal",
+    options: {
+      a: "texto",
+      b: "texto",
+      c: "texto",
+    },
+  },
+  {
+    content: "Descripcion",
+    type: "normal",
+    options: {
+      a: "texto",
+      b: "texto",
+      c: "texto",
+    },
+  },
+  {
+    content: "Descripcion",
+    type: "normal",
+    options: {
+      a: "texto",
+      b: "texto",
+      c: "texto",
+    },
+  },
+];
+
+export { cards };
